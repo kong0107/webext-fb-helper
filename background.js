89906602b0b86d74265fba587b4269eeeb9d17cd
kong0107 <kong0107@gmail.com>
2022-08-19 22:28:48 +0800

    initialization

--- background.js
+++ background.js
@@ -1,18 +1,14 @@
 const listeners = {
-    template: (request, sender, sendResponse) => {
-        console.debug(`an object is sent from tab ${sender.tab.id}:`, request);
-        return setTimeout(() => sendResponse("async func is supported if listener returns true"), 100);
-    },
     loadContentScript: ({filename}, {tab}) => {
         return chrome.scripting.executeScript({
             target: {tabId: tab.id},
             files: [filename]
         });
     },
-    // download: chrome.downloads.download, // need permission `downloads`
-    // downloadAll: ({dlOptArr}) => {
-    //     return Promise.allSettled(dlOptArr.map(chrome.downloads.download));
-    // }
+    download: chrome.downloads.download,
+    downloadAll: ({dlOptArr}) => {
+        return Promise.allSettled(dlOptArr.map(chrome.downloads.download));
+    }
 };
 chrome.runtime.onMessage.addListener(function (request) {
     if(listeners.hasOwnProperty(request.command))
@@ -20,8 +16,16 @@ chrome.runtime.onMessage.addListener(function (request) {
     console.error("unknown command " + request.command);
 });
 
-
-chrome.action.onClicked.addListener(tab => {
-    console.debug("This is executed when user clicks the extension logo.");
-    // listeners.loadContentScript({filename: "content_script.js"}, {tab});
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.action.disable();
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: {hostSuffix: ".facebook.com"}
+                })
+            ],
+            actions: [new chrome.declarativeContent.ShowAction()]
+        }]);
+    });
 });
